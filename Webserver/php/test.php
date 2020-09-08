b52f82012b1a9cb0ddf7a403aef07f52514176c2
imTJK <tjorvenkoopmann@gmail.com>
2020-09-08 18:07:17 +0200

    paa

---
+++ Webserver/php/test.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+
+        Welcome <?php echo $_POST["vorname"]; ?><br>
+        Your email address is: <?php echo $_POST["nachname"]; ?>
+
+    </body>
+</html>
---
+++ Webserver/start.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, url_for, redirect, render_template
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/testLink')
+def testLink():
+    name = "Hassan"
+    return render_template('testLink.html', **locals())
+
+@app.route('/test.php')
+def php():
+    render_template('')
+
+if __name__ == '__main__':
+    app.run()
