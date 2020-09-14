53d2e8700b060b306b5e11e1082a3e7673c552b1
imTJK <tjorvenkoopmann2001@gmail.com>
2020-09-14 17:47:46 +0200

    Kill em dead

--- Webserver/static/js/basic.js
+++
@@ -1,8 +0,0 @@
-function setName(){
-    var sex = document.getElementById('sexuality').value;
-    document.querySelector('#MomName').textContent = sex;
-}
-
-window.onload = function (){
-    document.getElementById('userErstellen').onclick = setName;
-}
--- Webserver/static/php/test.php
+++
@@ -1,8 +0,0 @@
-<html>
-    <body>
-
-        Welcome <?php echo $_POST["vorname"]; ?><br>
-        Your email address is: <?php echo $_POST["nachname"]; ?>
-
-    </body>
-</html>
