c06ca85b17c225bd15cf15ee4c67fd4bce386af0
imTJK <tjorvenkoopmann2001@gmail.com>
2020-09-15 23:29:58 +0200

    Homework - 15.09.2020

--- Webserver/__init__.py
+++ Webserver/__init__.py
@@ -1,8 +1,12 @@
 from flask import Flask, render_template
+from flask_login import LoginManager
 from Webserver.config import Config
 
 
 app = Flask(__name__)
 app.config.from_object(Config)
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 
 from Webserver import routes
--- Webserver/routes.py
+++ Webserver/routes.py
@@ -1,12 +1,25 @@
-from flask import render_template
+from flask import render_template, abort, url_for, request
 from Webserver import app
 
 @app.route('/')
-@app.route('/index')
+@app.route('/index', methods=['GET'])
 def index():
     user = {'username' : 'Hassan'}
-    return render_template('index.html', user=user)
+    return render_template('index.html', user=user, css_link=url_for('static', filename='css/index.css')) 
+    #url_for erlaubt website-spezifisches css und anwendung von html-vererbung zusammen
 
-@app.route('/item/<int:item_id>')
-def items(item_id):
-    return 'This is the ' + str(item_id) + ' Item'
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    abort(401)
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    abort(401)
+
+@app.route('/entry/new-Entry', methods=['POST', 'GET'])
+def new_entry():
+    abort(401)
+
+@app.route('/entry/<int:entry_id>',  methods=['GET'])
+def show_entry(entry_id):
+    abort(401)
